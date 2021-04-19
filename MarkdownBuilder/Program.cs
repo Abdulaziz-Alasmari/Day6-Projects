1038d726f0611f41e41335313bd08c60cb9dd1a5
Abdulaziz Alasmari <abdulaziz.alasmari@tuwaiq.edu.sa>
2021-04-19 04:59:47 +0300

    commit the whole project (to push it)

---
+++ MarkdownBuilder/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Builder
+{
+    
+    class MarkdownBuilder
+    {
+        private string markdown;
+
+        public MarkdownBuilder()
+        {
+            markdown = "";
+        }
+
+        public MarkdownBuilder addCustomizedTag( string tag )
+        {
+            markdown += tag;
+            return this;
+        }
+        public MarkdownBuilder addString(  string content, int size = 0)
+        {
+            markdown += "\n";
+            for( int i = 0; i < size; ++i)
+                markdown += "#";
+
+            markdown += " " + content + "  \n";
+
+            return this;
+        }
+        
+        public MarkdownBuilder addNewLine()
+        {
+             markdown += "\n";
+            return this;
+        }
+        public MarkdownBuilder addBoldedString( string content)
+        {
+            markdown += "**" + content + "**  ";
+            return this;
+        }
+        public MarkdownBuilder addItalicString( string content)
+        {
+             markdown += "*" + content + "*";
+            return this;
+        }
+        public MarkdownBuilder addBlockqoute( string content)
+        {
+            
+            markdown += "\n > " + content + "  \n";
+            return this;
+        }
+        public MarkdownBuilder addOLitem( string [] items )
+        {
+            
+            for( int i = 0; i < items.Length; ++i )
+            {
+                markdown += (i+1) + ". " + items[i] + "  \n";   
+            }
+            markdown += "\n";
+            return this;
+        }
+        public MarkdownBuilder addULitem( string [] items)
+        { 
+            
+            for( int i = 0; i < items.Length; ++i)
+            {
+                markdown += "- " + items[i] + "  \n";   
+            }
+            markdown += "\n";
+            return this;
+        }
+        public MarkdownBuilder addCode( string content)
+        {
+            markdown += "`" + content + "`";
+             return this;
+        }
+        public MarkdownBuilder addHorizontalLine()
+        { 
+            markdown += "\n ---- \n";
+            return this;
+        }
+        public MarkdownBuilder addLink( string title, string url)
+        { 
+            markdown += "\n[" + title + "]" + "(" + url + ")  \n";
+            return this;
+        }
+        public MarkdownBuilder addImage( string altText, string source)
+        {
+            markdown += "\n![" + altText + "]" + "(" + source + ")  \n";
+            return this;
+        }
+
+        public string get()
+        {
+            return markdown;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MarkdownBuilder md = new MarkdownBuilder();
+
+            string result = md.addCustomizedTag("<div dir =\"rtl\"> ")
+            .addNewLine()
+            .addString("وصف المشروع :", 2)
+            .addHorizontalLine()
+            .addString("المشروع ينقسم الى قسمين:" , 3)
+            .addNewLine()
+            .addULitem(new string[]{"Find matches:"})
+            .addString("هذا القسم يقوم بتحليل مدخل من نوع string لايجاد المتشابه من الرموز. هنا الرموز هي عبارة عن ارقام ")
+            .addNewLine()
+            .addULitem(new string[]{"String builder:"})
+            .addString("في هذا القسم حاولت تطبيق فكرة method chaining و بها بنيت builder للـ Markdown.")
+            .addNewLine()
+            .addString(" ملاحظة: تم بناء هذه الصفحة بواسطة إستخدام الـ markdown builder.")
+            .addNewLine()
+            .addCustomizedTag("</div>").get();
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Matches/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace Matches
+{
+    
+    class Program
+    {
+
+    public static bool isAllClosed( string source )
+    {
+        if( source == null || source.Length < 2 ) return false;
+        
+        char [] stack = new char[source.Length]; 
+        int stackPtr = 0;
+        foreach (var ch in source)
+        {
+            if( ! Char.IsWhiteSpace( ch ))
+            {
+                if( (stackPtr - 1) >= 0 )
+                {
+                    if( stack[stackPtr-1] != ch)
+                    {
+                        stack[stackPtr++] = ch;
+                    }
+                    else
+                    {
+                        stackPtr--;
+                    }
+                }
+                else
+                {
+                    stack[stackPtr++] = ch;
+                }
+            }
+        }
+        return stackPtr == 0;
+    }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(isAllClosed("123321"));
+            // Console.WriteLine(isAllClosed("11223131"));
+            // Console.WriteLine(isAllClosed("21"));
+            // Console.WriteLine(isAllClosed(""));
+            // Console.WriteLine(isAllClosed("5"));
+            // Console.WriteLine(isAllClosed("11221331"));
+            Console.WriteLine(isAllClosed("1233442881"));
+        }
+    }
+}
